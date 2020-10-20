63521c14287d09ccd00813ff7ed91f9814db60dc
nbthanh90 <71346718+nbthanh90@users.noreply.github.com>
2020-10-20 16:10:57 +0700

    nn

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace GiaiPhuongTrinhBacNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double a, b;
+            Console.WriteLine("Phương trình aX + b = 0");
+            Console.Write("Xin hãy nhập giá trị a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Xin hãy nhập giá trị b: ");
+            b = double.Parse(Console.ReadLine());
+            if (a != 0 && b != 0)
+            {
+                Console.WriteLine("Nghiệm của phương trình: X = " + -b / a);
+            }
+            else if (a != 0 && b == 0)
+            {
+                Console.WriteLine("Phương trình vô nghiệm!");
+            }
+            else if (a == 0 && b != 0)
+            {
+                Console.WriteLine("Nghiệm của phương trình: X = " + -b);
+            }
+            else
+            {
+                Console.WriteLine("X có vô số nghiệm!");
+            }
+        }
+    }
+}
